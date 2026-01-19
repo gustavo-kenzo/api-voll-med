535ef77f965d64851fd3563b173e50909a2f4811
Gustavo <sgkenzo212007@gmail.com>
2026-01-19 11:57:09 -0300

    feat: create User entity and configure database persistence

---
+++ src/main/java/med/voll/api_rest/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package med.voll.api_rest.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
